52da7de7619d658c1a6aaf59ac334d5bddeec9d0
kaedeair <34467508+kaedeair@users.noreply.github.com>
2024-05-25 13:55:16 +0800

    feat: add data define of file

---
+++ src/data/file.ts
@@ -0,0 +1,50 @@
+/*
+ * linkerfs-warp-gen-webui: warp configuration file generator webui for project linkerfs
+ * Copyright (C) 2024  kaedeair <kaedeair@outlook.com>
+ *
+ * This file is part of linkerfs-warp-gen-webui.
+ *
+ * linkerfs-warp-gen-webui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * linkerfs-warp-gen-webui is distributed in the hope that it will be useful,but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with linkerfs-warp-gen-webui. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export class FileInfo {
+    name: string
+    parentDir: string
+    size: number
+
+    constructor(name: string, parentDir: string, size: number) {
+        this.name = name
+        this.parentDir = parentDir
+        this.size = size
+    }
+}
+
+export class FileTree {
+    name: string
+    parentDir: string
+    childDir?: FileTree[]
+    childFile?: FileInfo[]
+    children?: FileTree[]
+
+    constructor(name: string, parentPath: string, childDir: FileTree[], childFile: FileInfo[]) {
+        this.name = name;
+        this.parentDir = parentPath
+        this.childDir = childDir
+        this.childFile = childFile
+        this.children = ((): FileTree[] => {
+            return this.childDir.concat(this.childFile)
+        })()
+    }
+}
